0ddef75f57dd0f29218e03f75a5e2a8d65410a5d
apease-9 <76882988+apease-9@users.noreply.github.com>
2021-03-22 13:12:05 -0400

    added functionality to burger

--- src/header-stuff/Header.js
+++ src/header-stuff/Header.js
@@ -7,30 +7,8 @@ import Form from "./Form.js";
 import "./Header.css";
 
 export default function Header(props) {
-  /* let list = document.querySelector(".header-stuff");
-  let burger = props.ani;
-  if (burger === "shrink") {
-    list.animate(
-      [{ transform: "translateX(0)" }, { transform: "translateX(100%)" }],
-      {
-        duration: 1000,
-        easing: "cubic-bezier(0.42, 0, 0.58, 1)",
-      }
-    );
-    list.style.transform = "translateX(100%)";
-  } else {
-    list.animate(
-      [{ transform: "translateX(100%)" }, { transform: "translateX(0)" }],
-      {
-        duration: 1000,
-        easing: "cubic-bezier(0.42, 0, 0.58, 1)",
-      }
-    );
-    list.style.transform = "translateX(0)";
-  }
-*/
   return (
-    <div className="header-stuff">
+    <nav className={`header-stuff ${props.ani}h`}>
       <Units />
       <Time />
       <Animations />
@@ -40,6 +18,6 @@ export default function Header(props) {
       <br />
       <Form />
       <br />
-    </div>
+    </nav>
   );
 }
